a819b5b7464a6fb8f3420bd2335400a82d3606e3
Wilhelm_Carmevik <carmevik@kth.se>
2024-06-21 15:08:26 +0800

    added data analysis for misc

--- data_analysis_misc.py
+++ data_analysis_misc.py
@@ -18,28 +18,26 @@ def open_and_plot(filename, column_index):
 def get_value(filename, column_index):
     (x_values, y_values) = get_xy(filename, column_index)
 
+    #Check these parameters
     r = 5000
-    center = 30000
+    center = 30000                          
     interval = y_values[center-r:center+r]
 
     y_max = max(interval)
 
     return y_max
 
-
 def get_standard_error(filename, column_index, start, stop):
     (x_values, y_values) = get_xy(filename, column_index)
 
     N = stop - start
 
-    values = np.array(y_values[start.stop])
+    values = np.array(y_values[start:stop])
 
     std = np.std(values)
     mean = np.mean(values)
 
     return (mean, std)
-
-
 
 def get_xy(filename, column_index):
     f = open(filename, "r")
@@ -59,12 +57,232 @@ def get_xy(filename, column_index):
     return (x_values, y_values)
 
 
+### OXYGEN ################################################################################################
 def O2_exhaled():
 
     calibration = get_value("O2_exhaled/O2-40mA-8008omega-23.1deg-49%RH-air-WMS/0.txt", 3)
 
+    m = get_value("O2_exhaled/O2-40mA-8008omega-23.1deg-49%RH-air-WMS-martin/0.txt", 3)
+    m1 = get_value("O2_exhaled/O2-40mA-8008omega-23.1deg-49%RH-air-WMS-martin1/0.txt", 3)
+    m2 = get_value("O2_exhaled/O2-40mA-8008omega-23.1deg-49%RH-air-WMS-martin2/0.txt", 3)
+
+    w = get_value("O2_exhaled/O2-40mA-8008omega-23.1deg-49%RH-air-WMS-wilhelm/0.txt", 3)
+    w1 = get_value("O2_exhaled/O2-40mA-8008omega-23.1deg-49%RH-air-WMS-wilhelm1/0.txt", 3)
+
+    air_conc = 0.18 #change
+
+    martin = air_conc * m / calibration
+    martin1 = air_conc * m1 / calibration
+    martin2 = air_conc * m2 / calibration
+
+    wilhelm = air_conc * w / calibration
+    wilhelm1 = air_conc * w1 / calibration
+
+    print("martin", martin)
+    print("martin1", martin1)
+    print("martin2", martin2)
+
+    print("wilhelm1", wilhelm)
+    print("wilhelm2", wilhelm1)
 
 
-print(get_value("O2_exhaled/O2-40mA-8008omega-23.1deg-49%RH-air-WMS-martin/0.txt", 3))
 
-open_and_plot("O2_exhaled/O2-40mA-8008omega-23.1deg-49%RH-air-WMS-martin/0.txt", 3)
+### CARBON DIOXIDE ################################################################################################
+def CO2_gasmix():
+    """
+    CO2 tank with 3000ppm
+    N2 tank with pure nitrogen gas
+
+    N         CO2         Concentration
+    600       0           0
+    500       100         500
+    400       200         1000
+    300       300         1500
+    200       400         2000
+    100       500         2500
+    0         600         3000
+    """
+
+    Concentration = [0, 500, 1000, 1500, 2000, 2500, 3000]
+
+    Amplitude = []
+
+    #Amplitude.append(get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N600/0.txt", 0))
+    #Detection limit. Need to take average
+
+    center = 30000
+    r = 1000
+    (mean, std) = get_standard_error("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N600/0.txt",0,center-r, center+r)
+
+    Amplitude.append(mean)
+
+    Amplitude.append(get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N500-O100/0.txt", 0))
+    Amplitude.append(get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N400-O200/0.txt", 0))
+    Amplitude.append(get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N300-O300/0.txt", 0))
+    Amplitude.append(get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N200-O400/0.txt", 0))
+    Amplitude.append(get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N100-O500/0.txt", 0))
+    Amplitude.append(get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N000-O600/0.txt", 0))
+
+    plt.plot(Concentration, Amplitude, marker="*")
+
+    plt.xlabel("Concentration [ppm]")
+    plt.ylabel("Amplitude [V]")
+    plt.title("Amplitude of detection vs concentration of CO2")
+
+    plt.show()
+
+
+def CO2_conc_in_air():
+    """Concentration in air in ppm"""
+    calibration = get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N000-O600/0.txt", 0)
+
+    air = get_value("CO2_exhaled/CO2-120mA-10000omega-23.1deg-49%RH-AIR-LOCKIN/0.txt", 0)
+
+    conc_in_air = 3000 * air / calibration
+
+    print(conc_in_air)
+
+
+def CO2_exhale():
+
+    calibration = get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N000-O600/0.txt", 0)
+
+    m = get_value("CO2_exhaled/CO2-120mA-10000omega-23.1deg-49%RH-MARTIN-LOCKIN/0.txt", 0)
+    w = get_value("CO2_exhaled/CO2-120mA-10000omega-23.1deg-49%RH-WILHELM-LOCKIN/0.txt", 0)
+    a = get_value("CO2_exhaled/CO2-120mA-10000omega-23.1deg-49%RH-ARON-LOCKIN/0.txt", 0)
+    
+    martin = 3000 * m / calibration
+    wilhelm = 3000 * w / calibration
+    aron = 3000 * a / calibration
+
+    print("Aron", aron)
+    print("Martin", martin)
+    print("Wilhelm", wilhelm)
+
+
+def CO2_detection_limit():
+    """
+    Detection limit for digital and analog lock-in amplifier in ppm
+    """
+
+    calibration_digital = get_value("CO2_detection_limit/CO2-120mA-10000omega-23.1deg-49%RH-3000ppm/0.txt", 3)
+    calibration_analog = get_value("CO2_detection_limit/CO2-120mA-10000omega-23.1deg-49%RH-3000ppm-LOCKIN/0.txt", 0)
+    
+    c_d = 30000
+    r_d = 1000
+    (limit_digital, std) = get_standard_error("CO2_detection_limit/N2-120mA-10000omega-23.1deg-49%RH-3000ppm-noise/0.txt",3,c_d-r_d, c_d+r_d)
+
+    c_a = 29000
+    r_a = 1000
+    (limit_analog, std) = get_standard_error("CO2_detection_limit/N2-120mA-10000omega-23.1deg-49%RH-3000ppm-LOCKIN-noise/0.txt",0,c_a-r_a, c_a+r_a)
+
+    dl_digital = 3000 * limit_digital / calibration_digital
+    dl_analog = 3000 * limit_analog / calibration_analog
+
+    print("Detection limit digital lock in amplifier", dl_digital, "ppm")
+    print("Detection limit analog lock in amplifier", dl_analog, "ppm")
+    # Weird values, recheck later
+
+### METHANE ################################################################################################
+
+def CH4_gasmix():
+    """
+    CH4 tank with 100ppm
+    N2 tank with pure nitrogen gas
+
+    N         CH4         Concentration
+    500       000         0
+    400       100         20
+    300       200         40
+    200       300         60
+    100       400         80
+    0         500         100
+    """
+
+    Concentration = [0, 20, 40, 60, 80, 100]
+
+    Amplitude = []
+
+    #Amplitude.append(get_value("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N600/0.txt", 0))
+    #Detection limit. Need to take average
+
+    center = 30000
+    r = 1000
+    (mean, std) = get_standard_error("CH4_gasmix_and_detection_limit/CH4-130mA-11000omega-22.4deg-77%RH-0PPM/0.txt",3,center-r, center+r)
+
+    Amplitude.append(mean)
+
+    Amplitude.append(get_value("CH4_gasmix_and_detection_limit/CH4-130mA-11000omega-22.4deg-77%RH-20PPM/0.txt", 3))
+    Amplitude.append(get_value("CH4_gasmix_and_detection_limit/CH4-130mA-11000omega-22.4deg-77%RH-40PPM/0.txt", 3))
+    Amplitude.append(get_value("CH4_gasmix_and_detection_limit/CH4-130mA-11000omega-22.4deg-77%RH-60PPM/0.txt", 3))
+    Amplitude.append(get_value("CH4_gasmix_and_detection_limit/CH4-130mA-11000omega-22.4deg-77%RH-80PPM/0.txt", 3))
+    Amplitude.append(get_value("CH4_gasmix_and_detection_limit/CH4-130mA-11000omega-22.4deg-77%RH-100PPM-A0.1/0.txt", 3))
+    #Wrong name in data, last one different
+
+    plt.plot(Concentration, Amplitude, marker="*")
+
+    plt.xlabel("Concentration [ppm]")
+    plt.ylabel("Amplitude [V]")
+    plt.title("Amplitude of detection vs concentration of CH4")
+
+    plt.show()
+
+def CH4_detection_limit():
+    
+    calibration = get_value("CH4_gasmix_and_detection_limit/CH4-130mA-11000omega-22.4deg-77%RH-100PPM-A0.1/0.txt", 3)
+
+    center = 30000
+    r = 1000
+    (mean, std) = get_standard_error("CH4_gasmix_and_detection_limit/CH4-130mA-11000omega-22.4deg-77%RH-0PPM/0.txt",3,center-r, center+r)
+
+    detection_limit = 100 * mean / calibration
+
+    print("Detection limit of CH4 measurements", detection_limit, "ppm")
+
+
+def CH4_exhaled():
+    
+    calibration = get_value("CH4_exhaled/CH4-130mA-11000omega-22.4deg-77%RH-100PPM-A0.1/0.txt", 3)
+
+    air = get_value("CH4_exhaled/CH4-130mA-11000omega-22.4deg-77%RH-AIR/0.txt", 3)
+    martin = get_value("CH4_exhaled/CH4-130mA-11000omega-22.4deg-77%RH-MARTIN/0.txt", 3)
+
+    air_conc = 100 * air / calibration
+    martin_conc = 100 * martin / calibration
+
+    print("Concentration in air", air_conc, "ppm")
+    print("Exhaled by martin", martin_conc, "ppm")
+
+
+
+### VAPOR ################################################################################################
+
+def VAPOR_exhaled():
+    
+    calibration = get_value("VAPOR_exhaled/VAPOR-70mA-8008omega-22.4deg-77%RH/0.txt", 0)
+
+    aron = get_value("VAPOR_exhaled/VAPOR-70mA-8008omega-22.4deg-77%RH-ARON/0.txt", 0)
+    martin = get_value("VAPOR_exhaled/VAPOR-70mA-8008omega-22.4deg-77%RH-MARTIN/0.txt", 0)
+    wilhelm = get_value("VAPOR_exhaled/VAPOR-70mA-8008omega-22.4deg-77%RH-WILHELM/0.txt", 0)
+
+    calibration_conc = 2.7 #correct, look this up later
+
+    aron_conc = calibration_conc * aron / calibration
+    martin_conc = calibration_conc * martin / calibration
+    wilhelm_conc = calibration_conc * wilhelm / calibration
+
+    print("Exhaled by aron", aron_conc, "proc")
+    print("Exhaled by martin", martin_conc, "proc")
+    print("Exhaled by wilhelm", wilhelm_conc, "proc")
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+#open_and_plot("CO2_gasmix/CO2-120mA-10000omega-23.1deg-49%RH-N000-O600/0.txt", 0)
+#open_and_plot("CO2_exhaled/CO2-120mA-10000omega-23.1deg-49%RH-AIR-LOCKIN/0.txt", 0)
+#print(CO2_conc_in_air())    
+#open_and_plot("O2_exhaled/O2-40mA-8008omega-23.1deg-49%RH-air-WMS-martin/0.txt", 3)
+
