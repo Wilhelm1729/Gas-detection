1a6e67f3678bd78e64a34b3e7a26ae845c1eba3b
Wilhelm_Carmevik <carmevik@kth.se>
2024-06-24 12:01:43 +0800

    updated code

--- tdlas.py
+++ tdlas.py
@@ -32,14 +32,24 @@ def wavelength_DFP(current):
     """Returns the wavelength the laser emits given a current"""
     return 763.7 + 0.4 * current / 0.03
 
-def output_laser1(current, temp):
+def output_laser764(current, temp):
     """From datasheet for 764nm laser
     Units: mW, nm, mA
+    Some things might be wrong but we did not use this laser
     """
+    print(temp)
     power = 0 if current < 10 else (current-10)/5 #at temp 25 c
-    wavelength = 763.6 + 0.7 / 20 * (current - 15) #at temp 25 c
+    wavelength = 763.6 + 0.7 / 20 * (current - 15) + (763.9-763.4)/10 * (temp - 25)
+
     return (power, wavelength) 
 
+def output_laser761(current, temp):
+    """
+    Units: mW, nm, mA
+    """
+    power = 0 if current < 10 else 0.25 * (current-10) #at temp 25 c, approximate
+    wavelength = 760.95 + 0.015 * (current - 15) + (761.15-760.4) / 15 * (temp-35)
+    return (power,wavelength)
 
 # Absorption coefficient
 nu, coef = absorptionCoefficient_Lorentz(SourceTables=compound, Diluent={'air':1.0})
@@ -53,39 +63,60 @@ def gas_absorption(wavelength, L, c):
     return exp(-get_absorption_coefficient(wavelength) * L * c * A)
 
 
-def get_detection(current, path_length, concentration):
+def get_detection(current, temperature, path_length, concentration):
     """Calculates what is detected"""
-    (power, wavelength) = output_laser1(current, 25)
+    (power, wavelength) = output_laser761(current, temperature)
 
-    return power * gas_absorption(wavelength, path_length, concentration) #Unit V, rescaled
+    return power * gas_absorption(wavelength, path_length, concentration)
 
 
 def direct_absorption():
     """DIRECT ABSORPTION SPECTROSCOPY"""
     # Driving current
-    threshold_current = 10
-
-    sawtooth_amp = 20
+    sawtooth_amp = 0.05
     sawtooth_freq = 1
+
+    base_current = 40.5 + 1.8 #mA
+    temperature = 30.13 #from omega = 8008
 
     # Sawtooth wave
     t = np.linspace(0,1,500)
-    driving_current = 40 + threshold_current / 2 * (1 + scipy.signal.sawtooth(2 * np.pi * t))
-    
+
+    voltage_signal = sawtooth_amp / 2 * (1 + scipy.signal.sawtooth(2 * np.pi * t * sawtooth_freq))
+    driving_current = base_current + 50 * voltage_signal
+
     # Experiment parameters
     path_length = 44 # in cm
     concentration = 0.72*10**(-5) # mols per cm^3
 
     # Measured voltage
-    v = np.array([get_detection(current, path_length, concentration) for current in driving_current])
-
-    #plt.plot(t, driving_current)
+    v = np.array([get_detection(current, temperature, path_length, concentration) for current in driving_current])
+    """
     plt.plot(t, v)
-    #plt.plot(t, driving_current)
-
-    
-    
+    plt.title("Detector")
     plt.show()
+    """
+    
+    fig, axs = plt.subplots(2,2, figsize=(10, 10))
+
+    axs[0,0].plot(t, v)
+    axs[0,0].set_title("Detector")
+    axs[1,0].plot(t, driving_current)
+    axs[1,0].set_title("Driving current")
+
+    power = []
+    wavelength = []
+    for current in driving_current:
+        (p, w) = output_laser761(current, temperature)
+        power.append(p)
+        wavelength.append(w)
+    
+    axs[0,1].plot(t, wavelength)
+    axs[0,1].set_title("Wavelength variation")
+
+    plt.tight_layout()
+    plt.show()
+    
 
 def wavelength_modulation():
     """WAVELENGTH MODULATION SPECTROSCOPY"""
@@ -171,7 +202,8 @@ def plot_absortion_coefficient(l_min, l_max):
 
 
 def main():
-    plot_absortion_coefficient(763,765)
+    #plot_absortion_coefficient(760,762)
+    direct_absorption()
 
 if __name__ == "__main__":
     main()
